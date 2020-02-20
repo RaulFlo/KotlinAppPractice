07f5956e2466a9ce004f1b1cc28bb84a131b35ee
Raul Flores <raulflo2012@gmail.com>
2020-02-19 22:04:01 -0600

    Add ExampleAdapter and ViewHolder

---
+++ app/src/main/java/com/example/android/kotlinrecyclerviewexample/ExampleAdapter.kt
@@ -0,0 +1,43 @@
+package com.example.android.kotlinrecyclerviewexample
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.example_item.view.*
+
+class ExampleAdapter(private val exampleList: List<ExampleItem>) : RecyclerView.Adapter<ExampleAdapter.ExampleViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ExampleViewHolder {
+        //create a view and inflate it with the layout for that single row
+        val itemView = LayoutInflater.from(parent.context).inflate(R.layout.example_item,
+            parent, false)
+
+        return ExampleViewHolder(itemView)
+    }
+
+
+    override fun onBindViewHolder(holder: ExampleViewHolder, position: Int) {
+        val currentItem = exampleList[position]
+        //takes ViewHolder in position of the list and fills items to the views. also make sure to use
+        //the cache view references inside the ViewHolder
+        holder.imageView.setImageResource(currentItem.imageResource)
+        holder.textView1.text = currentItem.text1
+        holder.textView2.text = currentItem.text2
+
+
+    }
+
+    override fun getItemCount() = exampleList.size
+
+    class ExampleViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
+        //link to the ref ids of the views inside the single row layout
+        val imageView: ImageView = itemView.image_view
+        val textView1: TextView = itemView.text_view_1
+        val textView2: TextView = itemView.text_view_2
+
+
+    }
+}
