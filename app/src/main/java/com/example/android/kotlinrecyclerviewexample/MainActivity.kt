097729ffc41cedd22b0cc96c1bbb5c4f4d189e6f
Raul Flores <raulflo2012@gmail.com>
2020-06-16 14:59:39 -0500

    Add or Remove random item,notify adapter

--- app/src/main/java/com/example/android/kotlinrecyclerviewexample/MainActivity.kt
+++ app/src/main/java/com/example/android/kotlinrecyclerviewexample/MainActivity.kt
@@ -2,39 +2,60 @@ package com.example.android.kotlinrecyclerviewexample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import androidx.recyclerview.widget.LinearLayoutManager
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
+
+    private val exampleList = generateDummyList(500)
+    private val adapter = ExampleAdapter(exampleList)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val exampleList = generateDummyList(500)
-
         // call the recycler view using synthetic property
-        recycler_view.adapter = ExampleAdapter(exampleList)
+        recycler_view.adapter = adapter
         recycler_view.layoutManager = LinearLayoutManager(this)
         recycler_view.setHasFixedSize(true)
+    }
 
+    fun insertItem(view: View) {
+        val index = Random.nextInt(8)
+
+        val newItem = ExampleItem(
+            R.drawable.ic_android,
+            "new Item at Position $index",
+            "Line 2"
+        )
+
+        exampleList.add(index, newItem)  // add new item to list
+        adapter.notifyItemInserted(index)  //tell adapter of change, pass position of item changed
+
+    }
+
+    fun deleteItem(view: View) {
+        val index = Random.nextInt(8)
+
+        exampleList.removeAt(index)
+        adapter.notifyItemRemoved(index)
 
     }
 
 
-
-
     //creates a list  with different drawables
-    private fun generateDummyList(size: Int): List<ExampleItem>{
+    private fun generateDummyList(size: Int): ArrayList<ExampleItem> {
         val list = ArrayList<ExampleItem>()
 
-        for(i in 0 until size){
-            val drawable = when (i % 3){
+        for (i in 0 until size) {
+            val drawable = when (i % 3) {
                 0 -> R.drawable.ic_android
                 1 -> R.drawable.ic_directions
                 else -> R.drawable.ic_person
             }
-            val item = ExampleItem(drawable, "Item $i","Line 2")
+            val item = ExampleItem(drawable, "Item $i", "Line 2")
             list += item
         }
         return list
