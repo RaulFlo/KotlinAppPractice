6b538283f24861d50e656d63c6daae56a7f1b55c
Raul Flores <raulflo2012@gmail.com>
2020-07-07 22:32:42 -0500

    Add OnClickListener to ExampleViewHolder

--- app/src/main/java/com/example/android/kotlinrecyclerviewexample/ExampleAdapter.kt
+++ app/src/main/java/com/example/android/kotlinrecyclerviewexample/ExampleAdapter.kt
@@ -8,12 +8,18 @@ import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.example_item.view.*
 
-class ExampleAdapter(private val exampleList: List<ExampleItem>) : RecyclerView.Adapter<ExampleAdapter.ExampleViewHolder>() {
+class ExampleAdapter(
+    private val exampleList: List<ExampleItem>,
+    private val listener: OnItemClickListener
+) :
+    RecyclerView.Adapter<ExampleAdapter.ExampleViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ExampleViewHolder {
         //create a view and inflate it with the layout for that single row
-        val itemView = LayoutInflater.from(parent.context).inflate(R.layout.example_item,
-            parent, false)
+        val itemView = LayoutInflater.from(parent.context).inflate(
+            R.layout.example_item,
+            parent, false
+        )
 
         return ExampleViewHolder(itemView)
     }
@@ -26,18 +32,31 @@ class ExampleAdapter(private val exampleList: List<ExampleItem>) : RecyclerView.
         holder.imageView.setImageResource(currentItem.imageResource)
         holder.textView1.text = currentItem.text1
         holder.textView2.text = currentItem.text2
-
-
     }
 
     override fun getItemCount() = exampleList.size
 
-    class ExampleViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
+    inner class ExampleViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView),
+        View.OnClickListener {
         //link to the ref ids of the views inside the single row layout
         val imageView: ImageView = itemView.image_view
         val textView1: TextView = itemView.text_view_1
         val textView2: TextView = itemView.text_view_2
 
+        init {
+            itemView.setOnClickListener(this)
+        }
 
+        override fun onClick(p0: View?) {
+            val position = adapterPosition
+            if (position != RecyclerView.NO_POSITION) {
+                listener.onItemClick(position)
+            }
+        }
     }
+
+    interface OnItemClickListener {
+        fun onItemClick(position: Int)
+    }
+
 }
