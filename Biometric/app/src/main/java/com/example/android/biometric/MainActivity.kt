f6fab84447acc7dc8a7b883407339731c2797f6a
Raul Flores <raulflo2012@gmail.com>
2021-01-27 13:19:50 -0600

    checkBiometricSupport()

--- Biometric/app/src/main/java/com/example/android/biometric/MainActivity.kt
+++ Biometric/app/src/main/java/com/example/android/biometric/MainActivity.kt
@@ -1,11 +1,67 @@
 package com.example.android.biometric
 
+import android.app.KeyguardManager
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.hardware.biometrics.BiometricPrompt
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CancellationSignal
+import android.widget.Toast
+import androidx.annotation.RequiresApi
+import androidx.core.app.ActivityCompat
 
 class MainActivity : AppCompatActivity() {
+
+    private var cancellationSignal: CancellationSignal? = null
+
+    private val authenticationCallback: BiometricPrompt.AuthenticationCallback
+    get() =
+        @RequiresApi(Build.VERSION_CODES.P)
+        object : BiometricPrompt.AuthenticationCallback(){
+            override fun onAuthenticationError(errorCode: Int, errString: CharSequence?) {
+                super.onAuthenticationError(errorCode, errString)
+                notifyUser("Authentication error: $errString")
+            }
+
+            override fun onAuthenticationSucceeded(result: BiometricPrompt.AuthenticationResult?) {
+                super.onAuthenticationSucceeded(result)
+                notifyUser("Authentication success!")
+                startActivity(Intent(this@MainActivity,SecretActivity::class.java))
+            }
+        }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        checkBiometricSupport()
+
+    }
+
+    private fun checkBiometricSupport(): Boolean{
+        val keyguardManager = getSystemService(Context.KEYGUARD_SERVICE) as KeyguardManager
+
+        if(!keyguardManager.isKeyguardSecure){
+            notifyUser("Fingerprint authentication has not been enabled in settings")
+            return false
+        }
+
+        if(ActivityCompat.checkSelfPermission(this,android.Manifest.permission.USE_BIOMETRIC)
+            != PackageManager.PERMISSION_GRANTED){
+            notifyUser("Fingerprint authentication is not enabled")
+            return false
+        }
+        return if(packageManager.hasSystemFeature(PackageManager.FEATURE_FINGERPRINT)){
+            true
+        }else true
+
+    }
+
+
+    private fun notifyUser(message: String){
+        Toast.makeText(this,message,Toast.LENGTH_SHORT).show()
     }
 }
